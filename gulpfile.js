85211c9d2a2d0e04effcf6afc39e986356da8051
Khliupnev Vladimir <proksik49032@mail.ru>
2025-07-30 19:50:28 +0400

    feat: add hero block

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ const watcher = () => {
   gulp.watch(app.paths.srcScss, styles);
   gulp.watch(app.paths.srcFullJs, scripts);
   gulp.watch(`${app.paths.srcPartialsFolder}/*.html`, htmlInclude);
-  gulp.watch(`${app.paths.base.src}/*.html`, htmlInclude);
+  gulp.watch(`${app.paths.base.src}/**/*.html`, htmlInclude);
   gulp.watch(`${app.paths.resourcesFolder}/**`, resources);
   gulp.watch(`${app.paths.srcImgFolder}/**/**.{jpg,jpeg,png,svg}`, images);
   gulp.watch(`${app.paths.srcImgFolder}/**/**.{jpg,jpeg,png}`, webpImages);

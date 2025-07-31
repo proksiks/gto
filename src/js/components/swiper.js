33c91503417c4b0425e4b6dd8d98ec0bc736c41a
Khliupnev Vladimir <proksik49032@mail.ru>
2025-07-31 10:34:16 +0400

    feat: add mobile news

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -1,6 +1,6 @@
 import Swiper from "swiper";
-// import { Navigation, Pagination } from "swiper/modules";
-// Swiper.use([Navigation, Pagination]);
+import { Navigation, Pagination } from "swiper/modules";
+Swiper.use([Navigation, Pagination]);
 
 const swiper = new Swiper(".loop-items", {
   slidesPerView: "auto",
@@ -9,5 +9,14 @@ const swiper = new Swiper(".loop-items", {
     delay: 1,
     pauseOnMouseEnter: false,
     disableOnInteraction: true,
+  },
+});
+
+const newsSwiper = new Swiper(".news__slider", {
+  slidesPerView: 1.25,
+  spaceBetween: 16,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
   },
 });

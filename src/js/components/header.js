bcb455dd671002b3508bf822f0247a037779175a
Khliupnev Vladimir <proksik49032@mail.ru>
2025-07-31 13:38:03 +0400

    feat: add info block

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -3,7 +3,7 @@ const headerBurgrerEl = document.querySelector(".header-mobile__burger");
 const headerMenuEl = document.querySelector(".header-mobile__menu");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 0) {
+  if (window.scrollY > 90) {
     headerEl.classList.add("header_slide");
   } else {
     headerEl.classList.remove("header_slide");

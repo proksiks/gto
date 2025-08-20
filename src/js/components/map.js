53965f5e1f8321d18c8462ce976440c621e90cfb
Khliupnev Vladimir <proksik49032@mail.ru>
2025-08-20 19:26:13 +0500

    fix: try to fix map

--- src/js/components/map.js
+++ src/js/components/map.js
@@ -35,6 +35,11 @@ async function initMap() {
     YMapDefaultSchemeLayer,
     YMapDefaultFeaturesLayer,
   } = ymaps3;
+
+  ymaps3.import.registerCdn('https://cdn.jsdelivr.net/npm/{package}', [
+    '@yandex/ymaps3-default-ui-theme@0.0.19'
+  ]);
+
   const { YMapPopupMarker } = await ymaps3.import(
     "@yandex/ymaps3-default-ui-theme"
   );

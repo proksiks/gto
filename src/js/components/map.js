5b63195430a68a54a4ff9ec30c5b004771c6c1fa
Khliupnev Vladimir <proksik49032@mail.ru>
2025-08-28 17:08:29 +0500

    update map

--- src/js/components/map.js
+++ src/js/components/map.js
@@ -7,18 +7,21 @@ const mapEl = document.getElementById("map");
 async function initMap() {
   await ymaps3.ready;
 
-  const { YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker } = ymaps3;
+  const { YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker } =
+    ymaps3;
 
   ymaps3.import.registerCdn("https://cdn.jsdelivr.net/npm/{package}", [
     "@yandex/ymaps3-default-ui-theme@0.0.19",
-    "@yandex/ymaps3-clusterer@0.0.11"
+    "@yandex/ymaps3-clusterer@0.0.11",
   ]);
 
   const { YMapPopupMarker } = await ymaps3.import(
     "@yandex/ymaps3-default-ui-theme"
   );
 
-  const {YMapClusterer, clusterByGrid} = await ymaps3.import('@yandex/ymaps3-clusterer');
+  const { YMapClusterer, clusterByGrid } = await ymaps3.import(
+    "@yandex/ymaps3-clusterer"
+  );
 
   const map = new YMap(
     mapEl,
@@ -61,38 +64,65 @@ async function initMap() {
   });*/
 
   data.forEach((markerProp) => {
-    setPopupMarker(markerProp)
+    setPopupMarker(markerProp);
   });
 
+  let openedPopup = null;
+
   function setPopupMarker(markerProp) {
+    const contentEl = PopupContent(markerProp);
+
     const popupMarker = new YMapPopupMarker({
       coordinates: markerProp.coordinates,
       position: markerProp.position,
       show: false,
-      content: () => PopupContent(markerProp),
+      content: () => contentEl,
     });
     map.addChild(popupMarker);
 
-    const markerElement = document.createElement('span');
-    markerElement.className = 'icon-marker';
-    markerElement.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE --><path fill="currentColor" d="M12 12q.825 0 1.413-.587T14 10t-.587-1.412T12 8t-1.412.588T10 10t.588 1.413T12 12m0 10q-4.025-3.425-6.012-6.362T4 10.2q0-3.75 2.413-5.975T12 2t5.588 2.225T20 10.2q0 2.5-1.987 5.438T12 22"/></svg>'
+    let isShown = false;
+
+    const markerElement = document.createElement("span");
+    markerElement.className = "icon-marker";
+    markerElement.innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M12 12q.825 0 1.413-.587T14 10t-.587-1.412T12 8t-1.412.588T10 10t.588 1.413T12 12m0 10q-4.025-3.425-6.012-6.362T4 10.2q0-3.75 2.413-5.975T12 2t5.588 2.225T20 10.2q0 2.5-1.987 5.438T12 22"/></svg>';
 
     const marker = new YMapMarker(
       {
         coordinates: markerProp.coordinates,
-        anchor: [0.5, 1]
+        anchor: [0.5, 1],
       },
       markerElement
     );
-
-
     map.addChild(marker);
 
-    markerElement.onclick = () => {
-      popupMarker.update({
-        show: true,
-      })
+    function setPopupVisibility(next) {
+      isShown = next;
+      popupMarker.update({ show: isShown });
+      if (isShown) {
+        if (openedPopup && openedPopup !== popupMarker) {
+          openedPopup.update({ show: false });
+          openedPopup.__setLocalState && openedPopup.__setLocalState(false);
+        }
+        openedPopup = popupMarker;
+      } else if (openedPopup === popupMarker) {
+        openedPopup = null;
+      }
+    }
+
+    popupMarker.__setLocalState = (v) => {
+      isShown = v;
     };
+
+    markerElement.addEventListener("click", (e) => {
+      e.stopPropagation();
+      setPopupVisibility(!isShown);
+    });
+
+    contentEl.addEventListener("click", (e) => {
+      e.stopPropagation();
+      if (isShown) setPopupVisibility(false);
+    });
   }
 }
 

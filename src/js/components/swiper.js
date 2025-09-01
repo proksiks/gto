fa31fdfb1a3885bcd0305d86cad3568ce87e1fae
Khliupnev Vladimir <proksik49032@mail.ru>
2025-09-01 22:26:31 +0500

    try to fix swiper

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -15,48 +15,50 @@ const LOOP_ANIMATION = {
   autoplay: AUTO_PLAY,
 };
 
-Swiper.use([Navigation, Pagination]);
+document.addEventListener("DOMContentLoaded", () => {
+  Swiper.use([Navigation, Pagination]);
 
-new Swiper(".loop-items", LOOP_ANIMATION);
-new Swiper("#galleryTop", { ...LOOP_ANIMATION, spaceBetween: 16 });
-new Swiper("#galleryBottom", {
-  ...LOOP_ANIMATION,
-  spaceBetween: 16,
-  speed: 12000,
-});
-new Swiper("#partnersTop", { ...LOOP_ANIMATION, spaceBetween: 16 });
-new Swiper("#partnersBottom", {
-  ...LOOP_ANIMATION,
-  spaceBetween: 16,
-  speed: 12000,
-});
-new Swiper(".news__slider", {
-  slidesPerView: 1.1,
-  spaceBetween: 16,
-  pagination: {
-    el: ".swiper-pagination",
-    type: "progressbar",
-  },
-});
+  new Swiper(".loop-items", LOOP_ANIMATION);
+  new Swiper("#galleryTop", { ...LOOP_ANIMATION, spaceBetween: 16 });
+  new Swiper("#galleryBottom", {
+    ...LOOP_ANIMATION,
+    spaceBetween: 16,
+    speed: 12000,
+  });
+  new Swiper("#partnersTop", { ...LOOP_ANIMATION, spaceBetween: 16 });
+  new Swiper("#partnersBottom", {
+    ...LOOP_ANIMATION,
+    spaceBetween: 16,
+    speed: 12000,
+  });
+  new Swiper(".news__slider", {
+    slidesPerView: 1.1,
+    spaceBetween: 16,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+  });
 
-new Swiper(".about-disciplines__slider", {
-  slidesPerView: "auto",
-  spaceBetween: 16,
-  pagination: {
-    el: ".about-disciplines__pagination",
-    type: "progressbar",
-  },
-  navigation: {
-    nextEl: ".about-disciplines__button-next",
-    prevEl: ".about-disciplines__button-prev",
-  },
-});
+  new Swiper(".about-disciplines__slider", {
+    slidesPerView: "auto",
+    spaceBetween: 16,
+    pagination: {
+      el: ".about-disciplines__pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".about-disciplines__button-next",
+      prevEl: ".about-disciplines__button-prev",
+    },
+  });
 
-new Swiper(".news-page__slider", {
-  slidesPerView: "auto",
-  spaceBetween: 8,
-  pagination: {
-    el: ".swiper-pagination",
-    type: "progressbar",
-  },
+  new Swiper(".news-page__slider", {
+    slidesPerView: "auto",
+    spaceBetween: 8,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+  });
 });
